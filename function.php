9a7e39abd2062b2341a4b229f6bb415d663ff4bf
佐原雅幸 <swra3ms@MacBook-Pro-3.local>
2020-02-02 03:56:19 +0900

    本番環境に接続

--- function.php
+++ function.php
@@ -183,7 +183,7 @@
     }
   }
 
-  //DB接続
+  /*//DB接続
   function dbConnect(){
     //DBへの接続準備
     $dsn = 'mysql:dbname=LatestMovies;host=localhost;charset=utf8mb4';
@@ -197,8 +197,8 @@
     //PDOオブジェクトを生成（DBへ接続）
     $dbh = new PDO($dsn, $user, $password, $options);
     return $dbh;
-  }
-/*
+  }*/
+
   //本番環境
   function dbConnect(){
     $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
@@ -214,7 +214,6 @@
     $dbh = new PDO($dsn,$user,$password,$options);
     return $dbh;
   }
-  */
 
   function queryPost($dbh, $sql, $data){
     //クエリー作成
